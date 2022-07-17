87d22b4f1a2be836eca31e8a40a8247de283d419
Jasmin0010 <sumichip4035@naver.com>
2022-07-18 02:17:31 +0900

    settings

---
+++ songpider-traveling-app/src/App.js
@@ -0,0 +1,15 @@
+/*eslint-disable */
+import './App.css';
+import React, {Component, Fragment, useState} from 'react';
+import './components/navbar.js'
+
+//main
+function App(){
+  return(
+    <div className ="App">
+      <ResponsiveAppBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ songpider-traveling-app/src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'home.css';
+
+//home button
+function HomeButton(){
+    return(
+        <div></div>
+    );
+} 
+
+//recommended course bar  
+<Recommend>
+    <section>
+
+    </section>
+</Recommend>
